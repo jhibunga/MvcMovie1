58b30edaba0ceaddc6652bf2f3e3a062d6188f0e
JIHAN <JIHAN@LAPTOP-7IFNFRS3>
2024-01-10 11:44:09 +0800

    Add project files.

---
+++ Data/MvcMovie1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie1.Data
+{
+    public class MvcMovie1Context : DbContext
+    {
+        public MvcMovie1Context (DbContextOptions<MvcMovie1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
